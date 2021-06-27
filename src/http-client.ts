1cd8e5548c5c3d93b0c6d57313d281c3cae07334
Christopher Fields <chrisjr06@gmail.com>
2021-06-26 23:14:46 -0500

    fix: + patch method

--- src/http-client.ts
+++ src/http-client.ts
@@ -4,38 +4,43 @@ import axios, {
   AxiosResponse,
   AxiosError,
   AxiosPromise,
-} from 'axios'
+} from 'axios';
 
 // es6 class wrapper for axios
 class Axios {
-  instance: AxiosInstance
+  instance: AxiosInstance;
 
   constructor(config: AxiosRequestConfig = {}) {
-    this.instance = axios.create(config)
+    this.instance = axios.create(config);
   }
 }
 
 export interface IHttpClient {
-  instance: AxiosInstance
-  get<R = any>(url: string, config?: AxiosRequestConfig): AxiosPromise<R>
+  instance: AxiosInstance;
+  get<R = any>(url: string, config?: AxiosRequestConfig): AxiosPromise<R>;
   post<D = any, R = any>(
     url: string,
     data?: D,
     config?: AxiosRequestConfig,
-  ): AxiosPromise<R>
+  ): AxiosPromise<R>;
   put<D = any, R = any>(
     url: string,
     data?: D,
     config?: AxiosRequestConfig,
-  ): AxiosPromise<R>
-  delete<R = any>(url: string, config?: AxiosRequestConfig): AxiosPromise<R>
+  ): AxiosPromise<R>;
+  patch<D = any, R = any>(
+    url: string,
+    data?: D,
+    config?: AxiosRequestConfig,
+  ): AxiosPromise<R>;
+  delete<R = any>(url: string, config?: AxiosRequestConfig): AxiosPromise<R>;
 }
 
 // provides better optional typing for http methods than axios
 export class HttpClient extends Axios implements IHttpClient {
   async get<R = any>(url: string, config?: AxiosRequestConfig) {
-    const res = await this.instance.get<R>(url, config)
-    return res
+    const res = await this.instance.get<R>(url, config);
+    return res;
   }
 
   async post<D = any, R = any>(
@@ -43,8 +48,8 @@ export class HttpClient extends Axios implements IHttpClient {
     data?: D,
     config?: AxiosRequestConfig,
   ) {
-    const res = await this.instance.post<R>(url, data, config)
-    return res
+    const res = await this.instance.post<R>(url, data, config);
+    return res;
   }
 
   async put<D = any, R = any>(
@@ -52,15 +57,24 @@ export class HttpClient extends Axios implements IHttpClient {
     data?: D,
     config?: AxiosRequestConfig,
   ) {
-    const res = await this.instance.put<R>(url, data, config)
-    return res
+    const res = await this.instance.put<R>(url, data, config);
+    return res;
+  }
+
+  async patch<D = any, R = any>(
+    url: string,
+    data?: D,
+    config?: AxiosRequestConfig,
+  ) {
+    const res = await this.instance.patch<R>(url, data, config);
+    return res;
   }
 
   async delete<R = any>(url: string, config?: AxiosRequestConfig) {
-    const res: AxiosResponse<R> = await this.instance.delete(url, config)
-    return res
+    const res: AxiosResponse<R> = await this.instance.delete(url, config);
+    return res;
   }
 }
 
 // export axios types for ease
-export { AxiosError, AxiosPromise }
+export { AxiosError, AxiosPromise };
